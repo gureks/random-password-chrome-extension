85821334c759ea3f1c5630fcc0f5d23f2bc1c5de
Gurek Singh <gurek15033@iiitd.ac.in>
2016-10-15 14:02:59 +0530

    update popup.html

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
-var smallAlphabets =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*"];
-var capitalAlphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var weak =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var medium = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var strong = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var strongest = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*"];
 
 pwdlength = 8; //Take it as input
 
-function generator(pwdlength) {
+function simpleGenerator(pwdlength) {
 	var Password = '';
-	for(i=0; i <= pwdlength; i++) {
-		var temp = smallAlphabets[Math.floor(Math.random() * smallAlphabets.length)];
+	for(i=0; i < pwdlength; i++) {
+		var temp = weak[Math.floor(Math.random() * weak.length)];
 		Password = Password + temp;
 	}
-	return Password
+	document.getElementById("password").value = Password;
+}
+
+function mediumGenerator(pwdlength) {
+	var Password = '';
+	for(i=0; i < pwdlength; i++) {
+		var temp = medium[Math.floor(Math.random() * medium.length)];
+		Password = Password + temp;
+	}
+	document.getElementById("password").value = Password;
+}
+
+function strongGenerator(pwdlength) {
+	var Password = '';
+	for(i=0; i < pwdlength; i++) {
+		var temp = strong[Math.floor(Math.random() * strong.length)];
+		Password = Password + temp;
+	}
+	document.getElementById("password").value = Password;
+}
+
+function strongestGenerator(pwdlength) {
+	var Password = '';
+	for(i=0; i < pwdlength; i++) {
+		var temp = strongest[Math.floor(Math.random() * strongest.length)];
+		Password = Password + temp;
+	}
+	document.getElementById("password").value = Password;
+}
